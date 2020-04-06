f681b09d0b65006a75c27bdfa7762edf3cae91ed
jmajurel <jeanfelix.majurel@gmail.com>
2020-04-06 13:16:54 +0200

    added authentification to getAll patient route

--- src/routers/patients.ts
+++ src/routers/patients.ts
@@ -3,6 +3,7 @@ import IPatientService from "../services/interfaces/patientServiceInterface";
 import { Request, Response } from "express";
 import bodyParser from "body-parser";
 import Patient from "../models/patient";
+import checkJwt from "./auth";
 
 @route("/patients")
 export default class PatientAPI {
@@ -10,10 +11,13 @@ export default class PatientAPI {
   constructor({ patientService }: { patientService: IPatientService }) {
     this._patientService = patientService;
   }
+
   @GET()
+  @before([checkJwt])
   async getAll(req: Request, res: Response) {
     return res.status(200).json(await this._patientService.getAll());
   }
+
   @POST()
   @before([bodyParser()])
   async post(req: Request, res: Response) {
